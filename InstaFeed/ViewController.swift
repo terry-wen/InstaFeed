b6df0db832bb33e6aab886db9c671042f7ed6c0a
Terry Wen <wen.terry.e@gmail.com>
2016-03-04 17:32:15 -0700

    Initial Commit

---
+++ InstaFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaFeed
+//
+//  Created by twen6 on 3/4/16.
+//  Copyright © 2016 twen6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
